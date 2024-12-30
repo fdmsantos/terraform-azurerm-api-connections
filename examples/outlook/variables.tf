5f94a00bf0513948b3833ee81c31b4824028e2b2
Yue He <joyceheyue@outlook.com>
2024-12-30 11:37:00 -0800

    remove variable default values

--- examples/outlook/variables.tf
+++ examples/outlook/variables.tf
@@ -1,7 +1,6 @@
 variable "name" {
   description = "Specifies the name of resource group."
   type        = string
-  default = "apimoduletest"
 }
 
 variable "location" {
@@ -13,5 +12,4 @@ variable "location" {
 variable "subscription_id" {
   description = "Specifies the subscription id should be used for this demo."
   type        = string
-  default = "43e47db3-18b4-4a8d-b9a7-565d9a07e57e"
 }
